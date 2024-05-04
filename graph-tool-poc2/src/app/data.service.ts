31bfcfc8df026e2c29f970f6ee253bff0dd2a132
rnsytmsk <rnsytmsk@gmail.com>
2024-05-04 19:48:04 +0900

    操作系をサービスに移動

--- graph-tool-poc2/src/app/data.service.ts
+++ graph-tool-poc2/src/app/data.service.ts
@@ -1,9 +1,220 @@
 import { Injectable } from '@angular/core';
+import { NodeEx } from './map-view/nodeex';
+import { EdgeEx } from './map-view/edgeex';
+import { ClusterNode, Edge } from '@swimlane/ngx-graph';
+
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  
+
+  public extendedNodes: NodeEx[] = [
+    {
+      id: '1',
+      label: 'Node 1',
+      isSelected: false,
+    }
+  ];
+  public get nodes(): NodeEx[] {
+    return this.extendedNodes;
+  }
+
+  private extendedEdges: EdgeEx[] = [];
+  public get links(): Edge[] {
+    return this.extendedEdges;
+  }
+
+  public clusters: ClusterNode[] = [];
+
+  // 状態(選択、入力、コマンド入力中)
+  private isInputState = false;
+  public get IsInputState(): boolean {
+    return this.isInputState;
+  }
+  public ChangeInputState() {
+    this.isInputState = !this.isInputState;
+  }
+  // 選択状態
+  private selectedNodeId = '';
+  // 追加コマンド状態
+  private isAddCommand = false;
+  public get IsAddCommand(): boolean {
+    return this.isAddCommand;
+  }
+  public ChangeAddCommandState() {
+    this.isAddCommand = true;
+  }
 
   constructor() { }
+
+
+  public SelectNode(nodeId: string) {
+    this.selectedNodeId = nodeId;
+    for(let i = 0; i < this.extendedNodes.length; i++){
+      this.extendedNodes[i].isSelected = false;
+      if (this.extendedNodes[i].id == nodeId){
+        this.extendedNodes[i].isSelected = true;
+      }
+    }
+    this.extendedNodes = [...this.extendedNodes];
+  }
+
+  public addNode() {
+    const nodeId = this.selectedNodeId;
+    for(let i = 0; i < this.extendedNodes.length; i++){
+      this.extendedNodes[i].isSelected = false;
+    }
+    
+    let selected_node_id = nodeId.toString();
+    let new_node_id = this.extendedNodes.length + 1;
+    let new_node = {
+      id: new_node_id.toString(),
+      label: `New Node ${new_node_id.toString()}`,
+      isSelected: true,
+    };
+    this.extendedNodes.push(new_node);
+
+    let edgeOrder = 0;
+    if (this.extendedEdges.length > 0) {
+      let sameSouorceEdges = this.extendedEdges.filter(edge => edge.source === selected_node_id);
+      if (sameSouorceEdges.length > 0) {
+        edgeOrder = Math.max(...sameSouorceEdges.map(edge => edge.order)) + 1;
+      }
+    }
+        
+    let new_link_id = `from_${selected_node_id}_to_${new_node_id}`;
+    let new_link: EdgeEx = {
+      id: new_link_id,
+      source: selected_node_id,
+      target: new_node_id.toString(),
+      label: 'is parent of',
+      order: edgeOrder,
+    };
+    this.extendedEdges = [...this.extendedEdges, new_link];
+    this.extendedNodes = [...this.extendedNodes];
+    this.selectedNodeId = new_node_id.toString();
+    console.log(this.extendedEdges);
+  }
+
+  // ノードを削除
+  public deleteNode() {
+    const nodeId = this.selectedNodeId
+
+      let parent_edges = this.extendedEdges.filter(edge => edge.target === nodeId);
+      if (parent_edges.length === 0) {
+        this.selectedNodeId = parent_edges[0].source;
+      }
+      // ノードを選択
+      for(let i = 0; i < this.extendedNodes.length; i++){
+        this.extendedNodes[i].isSelected = false;
+        if (this.extendedNodes[i].id == this.selectedNodeId){
+          this.extendedNodes[i].isSelected = true;
+        }
+      }
+  
+      
+      // ノードを削除
+      this.extendedNodes = this.extendedNodes.filter(node => node.id !== nodeId);
+      this.extendedNodes = [...this.extendedNodes];
+  
+      // 子ノードを列挙
+      let child_edges = this.extendedEdges.filter(edge => edge.source === nodeId);
+  
+      for(let child_edge of child_edges){
+        // 子ノードを削除
+        this.deleteNode();
+        // 子ノードとのリンクを削除
+        this.extendedEdges = [...this.extendedEdges.filter(edge => edge.id !== child_edge.id)];
+      }
+  
+      // 親ノードとのリンクを削除
+      for(let parent_edge of parent_edges){
+        // 子ノードとのリンクを削除
+        this.extendedEdges = this.extendedEdges.filter(edge => edge.id !== parent_edge.id);
+      }
+  
+      this.extendedEdges = [...this.extendedEdges];
+      this.extendedNodes = [...this.extendedNodes];
+    }
+
+  public MoveSelectedNodeLeft(){
+    for(let link of this.links){
+      if (link.target === this.selectedNodeId){
+        this.selectedNodeId = link.source;
+        for(let i = 0; i < this.extendedNodes.length; i++){
+          this.extendedNodes[i].isSelected = false;
+          if (this.extendedNodes[i].id == this.selectedNodeId){
+            this.extendedNodes[i].isSelected = true;
+          }
+        }
+        this.extendedNodes = [...this.extendedNodes];
+        return;
+      }
+    }
+  }
+
+  public MoveSelectedNodeRight(){
+    for(let link of this.links){
+      if (link.source === this.selectedNodeId){
+        this.selectedNodeId = link.target;
+        for(let i = 0; i < this.extendedNodes.length; i++){
+          this.extendedNodes[i].isSelected = false;
+          if (this.extendedNodes[i].id == this.selectedNodeId){
+            this.extendedNodes[i].isSelected = true;
+          }
+        }
+        this.extendedNodes = [...this.extendedNodes];
+        return;
+      }
+    }
+    return;
+  }
+
+  public MoveSelectedNodeUP () {
+    // 選択されたノードの親ノードと選択されたノードorderを取得
+    this.MoveNode('ArrowUp');
+  }
+
+  public ModeNodeDown() {
+    this.MoveNode('ArrowDown');
+  }
+
+
+  private MoveNode(moveDirection: string) {
+    let parent_node_id = '';
+    let selectedNodeOrderNumber = 0;
+    let parent_edge = this.extendedEdges.filter(edge => edge.target === this.selectedNodeId);
+    if (parent_edge.length > 0) {
+      parent_node_id = parent_edge[0].source;
+      selectedNodeOrderNumber = parent_edge[0].order;
+    }
+
+    // 選択されたノードの同列のノードでorderが1つ大きいノードを取得
+    let next_node_id = '';
+    let next_edge = moveDirection === 'ArrowDown' ?
+      this.extendedEdges.filter(edge => edge.source === parent_node_id && edge.order > selectedNodeOrderNumber) :
+      this.extendedEdges.filter(edge => edge.source === parent_node_id && edge.order < selectedNodeOrderNumber);
+    if (next_edge.length > 0) {
+      // 最小のorderを取得
+      let next_order = moveDirection === 'ArrowDown' ? Math.min(...next_edge.map(edge => edge.order)) :
+        Math.max(...next_edge.map(edge => edge.order));
+      let next_edge2 = this.extendedEdges.find(edge => edge.source === parent_node_id && edge.order === next_order);
+      if (next_edge2) {
+        let next_selected_node_id = next_edge2.target;
+        this.selectedNodeId = next_selected_node_id;
+        for (let i = 0; i < this.extendedNodes.length; i++) {
+          if (this.extendedNodes[i].id === next_selected_node_id) {
+            this.extendedNodes[i].isSelected = true;
+            continue;
+          }
+          this.extendedNodes[i].isSelected = false;
+          this.extendedEdges = [...this.extendedEdges];
+        }
+      }
+    }
+  }
 }
--- graph-tool-poc2/src/app/map-view/map-view.component.ts
+++ graph-tool-poc2/src/app/map-view/map-view.component.ts
@@ -31,25 +31,11 @@ export class MapViewComponent {
 
   public customLayout = new customLayout();
 
-  public extendedNodes: NodeEx[] = [
-    {
-      id: '1',
-      label: 'Node 1',
-      isSelected: false,
-    }
-  ];
-  public get nodes(): Node[] {
-    return this.extendedNodes;
-  }
 
-  private extendedEdges: EdgeEx[] = [];
-  public get links(): Edge[] {
-    return this.extendedEdges;
-  }
 
-  public clusters: ClusterNode[] = [];
+  constructor(public readonly DataService: DataService) {
 
-  constructor(readonly DataService: DataService) {
+
   }
 
   @HostListener('document:keypress', ['$event'])
@@ -59,10 +45,9 @@ export class MapViewComponent {
 
   @HostListener('document:keydown', ['$event'])
   handleKeydownEvent(event: KeyboardEvent) {
-    if (this.isInputState) {
+    if (this.DataService.IsInputState) {
       if (event.key === 'Escape') {
-        this.isInputState = false;
-        this.isAddCommand = false;
+        this.DataService.ChangeInputState();
         return;
       }
       return;
@@ -74,80 +59,30 @@ export class MapViewComponent {
     event.stopPropagation();
     event.preventDefault();
     if (event.key === 'Tab') {
-      this.addNode(parseInt(this.selectedNodeId));
+      this.DataService.addNode();
       return;
     }
     if (event.key === 'ArrowLeft') {
-      for(let link of this.links){
-        if (link.target === this.selectedNodeId){
-          this.selectedNodeId = link.source;
-          for(let i = 0; i < this.extendedNodes.length; i++){
-            this.extendedNodes[i].isSelected = false;
-            if (this.extendedNodes[i].id == this.selectedNodeId){
-              this.extendedNodes[i].isSelected = true;
-            }
-          }
-          this.extendedNodes = [...this.extendedNodes];
-          return;
-        }
-      }
+      this.DataService.MoveSelectedNodeLeft();
       return;
     }
     if (event.key === 'ArrowRight') {
-      for(let link of this.links){
-        if (link.source === this.selectedNodeId){
-          this.selectedNodeId = link.target;
-          for(let i = 0; i < this.extendedNodes.length; i++){
-            this.extendedNodes[i].isSelected = false;
-            if (this.extendedNodes[i].id == this.selectedNodeId){
-              this.extendedNodes[i].isSelected = true;
-            }
-          }
-          this.extendedNodes = [...this.extendedNodes];
-          return;
-        }
-      }
+      this.DataService.MoveSelectedNodeRight();
       return;
     }
 
-    if (event.key === 'ArrowDown' || event.key === 'ArrowUp') {
-      // 選択されたノードの親ノードと選択されたノードorderを取得
-      let parent_node_id = '';
-      let selectedNodeOrderNumber = 0;
-      let parent_edge = this.extendedEdges.filter(edge => edge.target === this.selectedNodeId);
-      if (parent_edge.length > 0) {
-        parent_node_id = parent_edge[0].source;
-        selectedNodeOrderNumber = parent_edge[0].order;
-      }
+    if (event.key === 'ArrowUp') {
+      this.DataService.MoveSelectedNodeUP();
+      return;
+    }
 
-      // 選択されたノードの同列のノードでorderが1つ大きいノードを取得
-      let next_node_id = '';
-      let next_edge = event.key === 'ArrowDown' ? 
-        this.extendedEdges.filter(edge => edge.source === parent_node_id && edge.order > selectedNodeOrderNumber) :
-        this.extendedEdges.filter(edge => edge.source === parent_node_id && edge.order < selectedNodeOrderNumber);
-      if (next_edge.length > 0) {
-        // 最小のorderを取得
-        let next_order = event.key === 'ArrowDown' ?  Math.min(...next_edge.map(edge => edge.order)) : 
-          Math.max(...next_edge.map(edge => edge.order));
-        let next_edge2 = this.extendedEdges.find(edge => edge.source === parent_node_id && edge.order === next_order);
-        if (next_edge2) {
-          let next_selected_node_id = next_edge2.target;
-          this.selectedNodeId = next_selected_node_id;
-          for(let i = 0; i < this.extendedNodes.length; i++){
-            if (this.extendedNodes[i].id === next_selected_node_id){
-              this.extendedNodes[i].isSelected = true;
-              continue;
-            }
-            this.extendedNodes[i].isSelected = false;
-            this.extendedEdges = [...this.extendedEdges];
-          }
-        }
-      }
+    if (event.key === 'ArrowDown') {
+      this.DataService.ModeNodeDown();
       return;
     }
 
     if (event.key === 'Delete') {
-      this.deleteNode(this.selectedNodeId);
+      this.DataService.deleteNode();
       return;
     }
 
@@ -155,55 +90,12 @@ export class MapViewComponent {
     event.stopPropagation();
     event.preventDefault();
   }
-
-  // ノードを削除
-  public deleteNode(nodeId: string) {
-
-    let parent_edges = this.extendedEdges.filter(edge => edge.target === nodeId);
-    if (parent_edges.length === 0) {
-      this.selectedNodeId = parent_edges[0].source;
-    }
-    // ノードを選択
-    for(let i = 0; i < this.extendedNodes.length; i++){
-      this.extendedNodes[i].isSelected = false;
-      if (this.extendedNodes[i].id == this.selectedNodeId){
-        this.extendedNodes[i].isSelected = true;
-      }
-    }
-
-    
-    // ノードを削除
-    this.extendedNodes = this.extendedNodes.filter(node => node.id !== nodeId);
-    this.extendedNodes = [...this.extendedNodes];
-
-    // 子ノードを列挙
-    let child_edges = this.extendedEdges.filter(edge => edge.source === nodeId);
-
-    for(let child_edge of child_edges){
-      // 子ノードを削除
-      this.deleteNode(child_edge.target);
-      // 子ノードとのリンクを削除
-      this.extendedEdges = [...this.extendedEdges.filter(edge => edge.id !== child_edge.id)];
-    }
-
-    // 親ノードとのリンクを削除
-    for(let parent_edge of parent_edges){
-      // 子ノードとのリンクを削除
-      this.extendedEdges = this.extendedEdges.filter(edge => edge.id !== parent_edge.id);
-    }
-
-    this.extendedEdges = [...this.extendedEdges];
-    this.extendedNodes = [...this.extendedNodes];
+  addNode() {
+    this.DataService.addNode();
   }
-
-
-  // 状態(選択、入力、コマンド入力中)
-  public isInputState = false;
-  private isAddCommand = false;
-  public selectedNodeId = '';
-
-
-
+  selectedNodeId(): any {
+    this.DataService.deleteNode();
+  }
 
   // ＋ボタンを押したときの処理
   onNodeKeypress($event: any, node: NodeEx) {
@@ -212,61 +104,15 @@ export class MapViewComponent {
 
   // ノードを選択したときの処理
   onNodeSelect($event: any) {
-    this.selectedNodeId = $event.id;
-    if (this.isAddCommand) {
-      this.isAddCommand = false;
-      this.addNode($event.id);
+    this.DataService.SelectNode($event.id);
+    if (this.DataService.IsAddCommand) {
+      this.DataService.addNode();
       return;
     }
-    for(let i = 0; i < this.extendedNodes.length; i++){
-      this.extendedNodes[i].isSelected = false;
-      if (this.extendedNodes[i].id == $event.id){
-        this.extendedNodes[i].isSelected = true;
-      }
-    }
-    this.extendedNodes = [...this.extendedNodes];
-  }
-
-  private addNode(nodeId: number) {
-    for(let i = 0; i < this.extendedNodes.length; i++){
-      this.extendedNodes[i].isSelected = false;
-    }
-    
-    let selected_node_id = nodeId.toString();
-    let new_node_id = this.extendedNodes.length + 1;
-    let new_node = {
-      id: new_node_id.toString(),
-      label: `New Node ${new_node_id.toString()}`,
-      isSelected: true,
-    };
-    this.extendedNodes.push(new_node);
-
-    let edgeOrder = 0;
-    if (this.extendedEdges.length > 0) {
-      let sameSouorceEdges = this.extendedEdges.filter(edge => edge.source === selected_node_id);
-      if (sameSouorceEdges.length > 0) {
-        edgeOrder = Math.max(...sameSouorceEdges.map(edge => edge.order)) + 1;
-      }
-    }
-        
-    let new_link_id = `from_${selected_node_id}_to_${new_node_id}`;
-    let new_link: EdgeEx = {
-      id: new_link_id,
-      source: selected_node_id,
-      target: new_node_id.toString(),
-      label: 'is parent of',
-      order: edgeOrder,
-    };
-    this.extendedEdges = [...this.extendedEdges, new_link];
-    this.extendedNodes = [...this.extendedNodes];
-    this.selectedNodeId = new_node_id.toString();
-
-
   }
 
   onNodeAdd($event: any) {
-    this.isAddCommand = true;
-    this.isInputState = true;
+    this.DataService.ChangeAddCommandState();
   }
 
   onKeypress($event: any) {
